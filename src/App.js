7b2342eb84e71bcae0c3bc4a7bbd4b3da4f131ca
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-27 10:52:02 +0100

    tasks can be added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
-import './App.css';
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField';
+
+
 
 class App extends Component {
+
+  state = {
+    tasks: [
+      { taskName: 'odkurzanie', completed: false },
+      { taskName: 'karmienie', completed: false }
+    ],
+    taskName: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({ taskName: event.target.value })
+  }
+  handleClick = () => {
+    let tasks = this.state.tasks;
+    tasks.push({ taskName: this.state.taskName, completed: false })
+    this.setState({ tasks })
+  }
   render() {
     return (
       <div className="App">
+        <div>
+          <TextField hintText="enter your task here" onChange={this.handleChange}
+          />
+          <RaisedButton label="add task" primary={true} onClick={this.handleClick} />
+        </div>
+        <div>
 
+          {this.state.tasks.map((task, index) => (
+            <div>{task.taskName}</div>
+          ))}
+        </div>
       </div>
     );
   }
