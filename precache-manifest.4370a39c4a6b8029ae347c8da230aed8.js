ae7af38a7edd1ad2accfe670c3a1e326fa20242a
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-27 14:54:12 +0100

    Updates

---
+++ precache-manifest.4370a39c4a6b8029ae347c8da230aed8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "4b81c124c2d33bece6a3",
+    "url": "/todo-app/static/js/main.4b81c124.chunk.js"
+  },
+  {
+    "revision": "6f6fd0a40f2d602c6443",
+    "url": "/todo-app/static/js/1.6f6fd0a4.chunk.js"
+  },
+  {
+    "revision": "d725e0744feba0c30deb3ae08c31cfb3",
+    "url": "/todo-app/index.html"
+  }
+];
