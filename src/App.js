c822ebf9cba6fed019c9a9ceaf9bcc96d82dc5a0
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-27 11:23:41 +0100

    fetching data from database and reading that date in console

--- src/App.js
+++ src/App.js
@@ -2,33 +2,56 @@ import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField';
 
-
+const API_URL = 'https://dawid-kurkiewicz.firebaseio.com'
 
 class App extends Component {
 
   state = {
-    tasks: [
-      { taskName: 'odkurzanie', completed: false },
-      { taskName: 'karmienie', completed: false }
-    ],
+    tasks: [],
     taskName: ''
+  }
+
+  componentWillMount() {
+    fetch(`${API_URL}/tasks.json`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('mam dane', data)
+      })
   }
 
   handleChange = (event) => {
     this.setState({ taskName: event.target.value })
   }
   handleClick = () => {
-    let tasks = this.state.tasks;
-    tasks.push({ taskName: this.state.taskName, completed: false })
-    this.setState({ tasks })
+    if (this.state.taskName !== '') {
+      let tasks = this.state.tasks;
+      const newTask = { taskName: this.state.taskName, completed: false }
+      fetch(`${API_URL}/tasks.json`, {
+        method: 'POST',
+        body: JSON.stringify(newTask)
+      }).then(() => {
+        tasks.push(newTask);
+        this.setState({ tasks, taskName: '' })
+      })
+
+    }
   }
+
+
+
   render() {
     return (
       <div className="App">
         <div>
-          <TextField hintText="enter your task here" onChange={this.handleChange}
+          <TextField
+            hintText="enter your task here"
+            value={this.state.taskName}
+            onChange={this.handleChange}
           />
-          <RaisedButton label="add task" primary={true} onClick={this.handleClick} />
+          <RaisedButton
+            label="add task"
+            primary={true}
+            onClick={this.handleClick} />
         </div>
         <div>
 
